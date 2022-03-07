e09bb1d6da56f80dc041d3071fccdd56db7ed22c
adam-elamrani <adelamco1091@outlook.com>
2022-03-07 16:54:14 +0100

    Added title to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Coinster</title>
+        <meta name="description" content="Seguimiento de criptoactivos" />
+      </Head>
       <h1>Coinster</h1>
       <p>Página en desarrollo...</p>
     </>
