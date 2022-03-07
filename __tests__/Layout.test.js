561a84e7d3ac07b315fddb0b45fee1b43d913ff8
adam-elamrani <adelamco1091@outlook.com>
2022-03-07 20:48:02 +0100

    Tested layout component. Established configs for testing/coverage

---
+++ __tests__/Layout.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import Layout from "../src/components/Layout/Layout";
+
+describe("Given a Layout component", () => {
+  describe("When it's invoked", () => {
+    test("Then it should print 'Test' in a heading", () => {
+      const expectedElement = "heading";
+      const expectedText = "Test";
+      render(<Layout />);
+
+      const expectedOutput = screen.getByRole(expectedElement, {
+        name: "Test",
+      });
+      console.log(expectedOutput);
+      expect(expectedOutput.textContent).toBe(expectedText);
+    });
+  });
+});
