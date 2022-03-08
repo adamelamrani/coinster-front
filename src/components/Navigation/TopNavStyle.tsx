9d61d62413c2b05e298a5e1f210ef4742cdeb17a
adam-elamrani <adelamco1091@outlook.com>
2022-03-08 10:02:59 +0100

    Added styled components to navigation bar

---
+++ src/components/Navigation/TopNavStyle.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const TopNavStyle = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    justify-content: space-evenly;
+  }
+`;
+
+export default TopNavStyle;
--- src/components/Navigation/TopNavigation.tsx
+++ src/components/Navigation/TopNavigation.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
+import TopNavStyle from "./TopNavStyle";
 
 const TopNavigation = () => {
   return (
     <>
-      <nav>
-        <Image src="coinster.png" alt="Coinster logo" />
-        <ul>
-          <li>Buscar</li>
+      <TopNavStyle className="top-nav-bar">
+        <Image
+          className="navigation-logo"
+          src="/coinster.png"
+          alt="Coinster logo"
+          width={100}
+          height={100}
+        />
+        <ul className="top-nav-list">
+          <li>
+            <input type="text" placeholder="Buscar..." />
+          </li>
           <li>Login</li>
         </ul>
-      </nav>
+      </TopNavStyle>
     </>
   );
 };
