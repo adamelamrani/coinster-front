5004cdfb534fbcc55bc639c2475c252af3115721
adam-elamrani <adelamco1091@outlook.com>
2022-03-14 16:19:56 +0100

    Create delete crypto thunk

--- src/redux/thunks/cryptoThunks.ts
+++ src/redux/thunks/cryptoThunks.ts
@@ -1,15 +1,29 @@
-import { loadCoinsAction } from "../actions/actionsCreator";
+import { deleteCoinsAction, loadCoinsAction } from "../actions/actionsCreator";
 import { Dispatch } from "redux";
 import { Cryptos } from "../../interfaces/cryptoProps";
-
-const url: string = `${process.env.NEXT_PUBLIC_COINSTER_API}/cryptos/list`;
+import Crypto from "../../interfaces/Crypto";
 
 export function loadCoinListThunk() {
   return async function (dispatch: Dispatch) {
-    const response = await fetch(url, {
-      method: "GET",
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_COINSTER_API}/cryptos/list`,
+      {
+        method: "GET",
+      }
+    );
     const cryptosList: Cryptos = await response.json();
     dispatch(loadCoinsAction(cryptosList));
   };
 }
+
+export const deleteCryptoThunk =
+  (id: number | string) => async (dispatch: Dispatch) => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_COINSTER_API}/cryptos/crypto/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const crypto: Crypto = await response.json();
+    dispatch(deleteCoinsAction(crypto.id));
+  };
