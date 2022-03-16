8fec55e9fc0e88251bfea8edbf80068b7e89ea78
adam-elamrani <adelamco1091@outlook.com>
2022-03-16 20:24:20 +0100

    Created create crypto formular

--- src/components/CreateCrypto/CreateCrypto.tsx
+++ src/components/CreateCrypto/CreateCrypto.tsx
@@ -1,4 +1,5 @@
 import React, { FormEventHandler, useState } from "react";
+import Button from "../Button/Button";
 
 interface Form {
   name: string;
@@ -52,6 +53,9 @@ const CreateCrypto: React.ReactNode = () => {
     formData.total_supply === 0 ||
     formData.platform === [] ||
     formData.price === 0 ||
+    formData.percent_change_1h === 0 ||
+    formData.percent_change_24h === 0 ||
+    formData.percent_change_7d === 0 ||
     formData.market_cap === 0 ||
     formData.img === "";
 
@@ -136,14 +140,62 @@ const CreateCrypto: React.ReactNode = () => {
         value={formData.market_cap}
         onChange={CreateCrypto}
       />
-      <label htmlFor="img_form">Capital de mercado:</label>
+      <label htmlFor="percent_change_1h_form">
+        Porcentaje de cambio última hora:
+      </label>
       <input
         type="number"
-        name="market_cap_form"
-        id="market_cap_form"
-        placeholder="Introduzca el capital de mercado"
-        value={formData.market_cap}
+        name="percent_change_1h_form"
+        id="percent_change_1h_form"
+        placeholder="Variación última hora"
+        value={formData.percent_change_1h}
         onChange={CreateCrypto}
+      />
+      <label htmlFor="percent_change_1h_form">
+        Porcentaje de cambio última hora:
+      </label>
+      <input
+        type="number"
+        name="percent_change_24h_form"
+        id="percent_change_24h_form"
+        placeholder="Variación últimas 24 horas"
+        value={formData.percent_change_24h}
+        onChange={CreateCrypto}
+      />
+      <label htmlFor="percent_change_1h_form">
+        Porcentaje de cambio últimos 7 días:
+      </label>
+      <input
+        type="number"
+        name="percent_change_7d_form"
+        id="percent_change_7d_form"
+        placeholder="Variación últimos 7 días"
+        value={formData.percent_change_7d}
+        onChange={CreateCrypto}
+      />
+      <label htmlFor="percent_change_1h_form">
+        Porcentaje de cambio última hora:
+      </label>
+      <input
+        type="number"
+        name="percent_change_24h_form"
+        id="percent_change_24h_form"
+        placeholder="Variación últimas 24 horas"
+        value={formData.percent_change_24h}
+        onChange={CreateCrypto}
+      />
+      <label htmlFor="img_form">Logo de su Cryptoactivo:</label>
+      <input
+        type="file"
+        name="img_form"
+        id="img_form"
+        value={formData.img}
+        onChange={CreateCrypto}
+      />
+      <Button
+        disableCondition={invalidForm}
+        actionOnClick={() => {}}
+        text={"Crear"}
       />
     </form>
   );
