844a5025846c9574b94e511000bdd366a552e46a
adam-elamrani <adelamco1091@outlook.com>
2022-03-21 22:39:25 +0100

    Installed toastify. Created cases

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -2,6 +2,8 @@ import { Dispatch } from "redux";
 import jwtDecode from "jwt-decode";
 import { loginAction, registerAction } from "../actions/actionsCreator";
 import Router from "next/router";
+import toastNotification from "../../utils/toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const loginThunk = (user: any) => async (dispatch: Dispatch) => {
   const response = await fetch(
@@ -19,6 +21,7 @@ export const loginThunk = (user: any) => async (dispatch: Dispatch) => {
     const token = await response.json();
     const { username }: any = await jwtDecode(token.token);
     localStorage.setItem("token", token.token);
+    toastNotification(`Bienvenido ${username}`, "success");
     dispatch(loginAction({ username, token: token.token }));
     Router.push("/");
   }
